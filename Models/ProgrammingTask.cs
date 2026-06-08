d35a7a09a8f6ce0835f4042e1ec37768469ec26b
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 09:26:25 -0500

    Feat: Se crea la clase ProgrammingTask

---
+++ Models/ProgrammingTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudyGo.Models
+{
+    public class ProgrammingTask : Activity
+    {
+        public string Language { get; set; }
+        public int TimeLimitSeconds { get; set; }
+        public int MemoryLimitMb { get; set; }
+
+        public Rubric Rubric { get; set; }
+        public ICollection<Submission> Submissions { get; set; } = new List<Submission>();
+    }
+}
