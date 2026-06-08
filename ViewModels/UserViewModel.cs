b37adedf85df9fe0a1f3208723a432a826129ddd
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:33:41 -0500

    Feat: Se crea el modelo UserViewModel

---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyGo.ViewModels
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "La institución es obligatoria.")]
+        [Display(Name = "Institución")]
+        public Guid InstitutionId { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
+        [StringLength(256, ErrorMessage = "El correo no puede superar los 256 caracteres.")]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre de usuario debe tener entre 2 y 100 caracteres.")]
+        [Display(Name = "Nombre de usuario")]
+        public string DisplayName { get; set; }
+    }
+}
