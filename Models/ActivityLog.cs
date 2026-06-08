ed56f6d895593d7a574c191b6f9abc881fae5c0e
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:13:29 -0500

    Feat: Se crea la clase ActivityLog

---
+++ Models/ActivityLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudyGo.Models
+{
+    public class ActivityLog
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Action { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public User User { get; set; }
+    }
+}
