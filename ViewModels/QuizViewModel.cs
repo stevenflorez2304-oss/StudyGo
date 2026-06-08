1a0047d5c7c285812da958c876a23b3f1e8053d8
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:45:44 -0500

    Feat: Se crea el modelo de QuizViewModel

---
+++ ViewModels/QuizViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using StudyGo.Enums;
+
+namespace StudyGo.ViewModels
+{
+    public class QuizViewModel : ActivityViewModel
+    {
+        [Required(ErrorMessage = "El modo de selección es obligatorio.")]
+        [Display(Name = "Modo de selección")]
+        public SelectionMode SelectionMode { get; set; }
+    }
+}
