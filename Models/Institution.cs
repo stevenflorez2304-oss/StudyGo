3e6c9f5739a5dd8a2888aa22568ea1897c9cefe3
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 09:14:29 -0500

    Feat: Se crea la clase Institution y se elimina el controlador por default

---
+++ Models/Institution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudyGo.Models
+{
+    public class Institution
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<User> Users { get; set; } = new List<User>();
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
