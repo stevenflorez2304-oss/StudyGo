0864a7cf2491396770c6c784da0af55d58d25cac
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 09:19:35 -0500

    Feat: Se crea la clase UserRole

---
+++ Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudyGo.Models
+{
+    public class UserRole
+    {
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+
+        public User User { get; set; }
+        public Role Role { get; set; }
+    }
+}
