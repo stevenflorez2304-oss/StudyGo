32a0cbaf0560d74f60ccf03389d36c86a09339d6
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:38:13 -0500

    Feat: Se crea el modelo de ActivityViewModel

---
+++ ViewModels/ActivityViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using StudyGo.Enums;
+
+namespace StudyGo.ViewModels
+{
+    public abstract class ActivityViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "El curso es obligatorio.")]
+        [Display(Name = "Curso")]
+        public Guid CourseId { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio.")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "El título debe tener entre 3 y 200 caracteres.")]
+        [Display(Name = "Título")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [StringLength(2000, MinimumLength = 10, ErrorMessage = "La descripción debe tener entre 10 y 2000 caracteres.")]
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio.")]
+        [Display(Name = "Estado")]
+        public ActivityState State { get; set; }
+    }
+}
