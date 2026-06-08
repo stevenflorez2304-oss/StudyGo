43e1e5eb3cebc52a62af7732d9530af9c9084378
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:11:56 -0500

    Feat: Se crea la clase CalendarEvent

---
+++ Models/CalendarEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudyGo.Models
+{
+    public class CalendarEvent
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public string Title { get; set; }
+        public DateTime StartsAt { get; set; }
+        public DateTime EndsAt { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
