3958ec53640103320a96166deee33101250805b4
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 11:08:00 -0500

    Feat: Se crea el modelo de SubmissionViewModel

---
+++ ViewModels/SubmissionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using StudyGo.Enums;
+
+namespace StudyGo.ViewModels
+{
+    public class SubmissionViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "La tarea es obligatoria.")]
+        [Display(Name = "Tarea de programación")]
+        public Guid ProgrammingTaskId { get; set; }
+
+        [Required(ErrorMessage = "El estudiante es obligatorio.")]
+        [Display(Name = "Estudiante")]
+        public Guid StudentId { get; set; }
+
+        [Required(ErrorMessage = "El estado es obligatorio.")]
+        [Display(Name = "Estado")]
+        public SubmissionStatus Status { get; set; }
+    }
+}
