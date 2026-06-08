5ba04ef6004498a2c4ed3dde92d96adb126a1d5f
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 12:00:24 -0500

    Feat: Se crea el modelo QuizAttemptViewModel

---
+++ ViewModels/QuizAttemptViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyGo.ViewModels
+{
+    public class QuizAttemptViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "El quiz es obligatorio.")]
+        [Display(Name = "Quiz")]
+        public Guid QuizId { get; set; }
+
+        [Required(ErrorMessage = "El estudiante es obligatorio.")]
+        [Display(Name = "Estudiante")]
+        public Guid StudentId { get; set; }
+
+        [Required(ErrorMessage = "El puntaje es obligatorio.")]
+        [Range(0, 100, ErrorMessage = "El puntaje debe estar entre 0 y 100.")]
+        [Display(Name = "Puntaje")]
+        public decimal Score { get; set; }
+
+        [Required(ErrorMessage = "La fecha de envío es obligatoria.")]
+        [Display(Name = "Enviado el")]
+        public DateTime SubmittedAt { get; set; }
+    }
+}
