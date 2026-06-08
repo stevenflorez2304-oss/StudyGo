5bb151e0de5add8adf435c0c6e37d039c2d000bd
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 09:23:24 -0500

    Feat: Se crea la clase Enrollment

---
+++ Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using StudyGo.Enums;
+
+namespace StudyGo.Models
+{
+    public class Enrollment
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public Guid CourseId { get; set; }
+        public Guid EnrolledBy { get; set; }
+        public DateTime EnrolledAt { get; set; }
+        public EnrollmentStatus Status { get; set; }
+
+        public User Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
