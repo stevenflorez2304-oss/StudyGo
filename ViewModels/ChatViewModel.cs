4bf55e154da2a5db4ec3e154e396238206638c62
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 12:02:24 -0500

    Feat: Se crea el modelo ChatViewModel

---
+++ ViewModels/ChatViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using StudyGo.Enums;
+
+namespace StudyGo.ViewModels
+{
+    public class ChatViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "El tipo de chat es obligatorio.")]
+        [Display(Name = "Tipo de chat")]
+        public ChatType Type { get; set; }
+    }
+}
