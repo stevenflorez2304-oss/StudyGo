423c749e637248cbb7578c8d2a8c6de6d75591c8
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:34:46 -0500

    Feat: Se crea el modelo RoleViewModel

---
+++ ViewModels/RoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyGo.ViewModels
+{
+    public class RoleViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del rol es obligatorio.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 100 caracteres.")]
+        [Display(Name = "Nombre del rol")]
+        public string Name { get; set; }
+    }
+}
