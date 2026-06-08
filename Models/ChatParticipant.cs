a642890e979e0888d6aecad096b5382fa87a633e
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2026-06-08 10:01:12 -0500

    Feat: Se crea la clase ChatParticipant

---
+++ Models/ChatParticipant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudyGo.Models
+{
+    public class ChatParticipant
+    {
+        public Guid ChatId { get; set; }
+        public Guid UserId { get; set; }
+
+        public Chat Chat { get; set; }
+        public User User { get; set; }
+    }
+}
